4a85b9552aa0e5c5c1823a4e77324a36b639c33e
silviolimeira <sicemal@gmail.com>
2023-04-30 10:58:26 -0300

    Adding More React Components & Connecting

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,34 @@
+import Link from "next/link";
+
 function EventItem(props) {
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+  const humanReadableDate = new Date(date).toLocaleDateString("pt-BR", {
+    day: "numeric",
+    monty: "long",
+    year: "numeric",
+  });
+  const formatAddress = location.replace(",", "\n");
+  const exploreLink = `/events/${id}`;
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div>
+          <time>{humanReadableDate}</time>
+        </div>
+        <div>
+          <address>{formatAddress}</address>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 function HomePage() {
-  const filteredEvents = getFeaturedEvents();
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <ul></ul>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
