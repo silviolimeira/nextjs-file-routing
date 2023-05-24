b1811b95816bb8898e880092d644b44936b6c291
silviolimeira <sicemal@gmail.com>
2023-05-23 21:00:40 -0300

    The _document.js File (And What It Does)

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import classes from "./event-item.module.css";
@@ -19,7 +20,7 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={340} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
